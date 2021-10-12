906d1317ba131b1f0ec1793d64ed5dbf5d07bf7b
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-10-11 21:45:21 -0300

    Update User.ts
    Criação do model User

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,30 @@
 import {Column, CreateDateColumn, Entity, PrimaryColumn} from 'typeorm'
-
+import {v4 as uuid} from 'uuid';
 
 
 @Entity("users")
 class User {
     
-    constructor(){}
+    constructor(){
+        if(!this.id){
+
+        }
+    }
+
+    @PrimaryColumn()
+    readonly id:string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    create_at: Date;
 
 }
 
