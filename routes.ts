893f53aefcdf081cee8b8a89959c94e061573e9f
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-10-15 20:49:35 -0300

    implemento de rotas

---
+++ routes.ts
@@ -0,0 +1,15 @@
+import {Router, Request, Response, response} from 'express';
+
+    interface UserRequest {
+        name: string;
+        email: string;
+        password: string;
+    }
+
+const routes = Router();
+
+routes.post('/user', () => console.log('User route'));
+routes.post('/activy', () => console.log('Activy route'));
+routes.post('/courseuunit', () => console.log('Course Unit route'));
+
+export default routes;
