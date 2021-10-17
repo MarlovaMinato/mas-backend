50e8e9b20ee4164046219223342de092d7879ef4
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-10-17 20:31:52 -0300

    Update AuthenticateController
    const authData

--- src/controller/AuthenticateController.ts
+++ src/controller/AuthenticateController.ts
@@ -1,5 +1,9 @@
+import {Request, Response} from "express";
+
 class AuthenticateController {
-    async create (){}
+    async create (request:Request, response:Response){
+        const authData = request.body;
+    }
 }
 
 export {AuthenticateController}
