69f253a4ecb53b97a0fe374b18bc7401c836495c
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-10-17 21:01:33 -0300

    Update AuthenticateUserService.ts

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,3 +1,8 @@
+import {compare} from "bcryptjs";
+import {sign} from 'jsonwebtoken';
+import {getRepository} from "typeorm"
+import {User} from "../model/User"
+import authConfig from '../config/auth'
 
 interface AuthData {
     email: string;
@@ -5,8 +10,34 @@ interface AuthData {
 }
 
 class AuthenticateUserService {
-    public async execute({email, password}: AuthData){
+    public async execute({email, password}: AuthData): Promise<String | {}>{
 
+        const usersRepository = getRepository(User)
+
+        const user = await usersRepository.findOne({email});
+
+        if(!user){
+            return {
+                error: 'user not found'
+             }
+        }
+
+
+        const comparePassword = compare(password, user.password)
+
+        if(!comparePassword){
+            return {
+                error: 'incorrect password'
+            }
+        }
+const {secret, expiresIn} = authConfig.jwt
+        
+        const token = sign({"role": "user"} , secret, {
+            subject: user.id,
+            expiresIn
+        });
+
+        return token;
     }
 }
 
